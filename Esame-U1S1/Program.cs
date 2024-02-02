7bad171292446e24dbaec1ddd248008a18e35000
MelissaCanu <melissamcanu@gmail.com>
2024-02-02 13:07:57 +0100

    added final controls and revised spacing and comments

--- Esame-U1S1/Program.cs
+++ Esame-U1S1/Program.cs
@@ -14,27 +14,31 @@ namespace Esame_U1S1
 
             Console.WriteLine("Inserisci i dati del contribuente");
 
+            // nome e cognome 
+
             string nome = "";
             do
+
             {
                 Console.WriteLine("Nome: ");
                 nome = Console.ReadLine();
-                if (string.IsNullOrEmpty(nome))
+                if (string.IsNullOrEmpty(nome)) // metodo statico per verificare che il nome non sia vuoto
                 {
                     Console.WriteLine("Inserire un valore");
                 }
-            } while (string.IsNullOrEmpty(nome)); // ciclo finché non si inserisce un nome corretto
+            } while (string.IsNullOrEmpty(nome)); 
 
            string cognome = "";
+
             do
             {
                 Console.WriteLine("Cognome: ");
                 cognome = Console.ReadLine();
-                if (string.IsNullOrEmpty(cognome))
+                if (string.IsNullOrEmpty(cognome)) // metodo statico per verificare che il cognome non sia vuoto
                 {
                     Console.WriteLine("Inserire un valore");
                 }
-            } while (string.IsNullOrEmpty(cognome)); // ciclo finché non si inserisce un cognome corretto
+            } while (string.IsNullOrEmpty(cognome)); 
 
             // data
 
@@ -48,7 +52,7 @@ namespace Esame_U1S1
 
                 try 
                 {
-                    dataNascita = DateTime.Parse(inputDataNascita);
+                    dataNascita = DateTime.Parse(inputDataNascita); // trasformo la stringa inserita in un oggetto DateTime
                     inputDataValido = true;
                 }
                 catch (FormatException) 
@@ -57,45 +61,60 @@ namespace Esame_U1S1
 
                 }
 
-            } while (!inputDataValido); // ciclo finché non si inserisce una data corretta
+            } while (!inputDataValido); 
 
             // codice fiscale
 
             string codiceFiscale = "";
+
             do
             {
                 Console.WriteLine("Codice fiscale: ");
-                codiceFiscale = Console.ReadLine().ToUpper();
+                codiceFiscale = Console.ReadLine().ToUpper(); // trasformo la stringa inserita in maiuscolo
                 if (codiceFiscale.Length != 16)
                 {
                     Console.WriteLine("Il codice fiscale deve essere lungo 16 caratteri");
                 }
-            } while (codiceFiscale.Length != 16); // ciclo finché non si inserisce un codice fiscale corretto
+            } while (codiceFiscale.Length != 16); 
 
             // sesso
 
             char sesso = ' ';
+
             do
             {
                 Console.WriteLine("Sesso: (f, m, x) ");
-                sesso = char.Parse(Console.ReadLine());
-                if (sesso != 'f' && sesso != 'm' && sesso != 'x')
+                string inputSesso = Console.ReadLine().ToLower(); // trasformo la stringa inserita in minuscolo
+
+                if (string.IsNullOrEmpty(inputSesso)) // metodo statico per verificare che il sesso non sia vuoto
+                {
+                    Console.WriteLine("Inserire un valore");
+                }
+                else if (inputSesso != "f" && inputSesso != "m" && inputSesso != "x")
                 {
                     Console.WriteLine("Il sesso deve essere f, m o x");
                 }
-            } while (sesso != 'f' && sesso != 'm' && sesso != 'x'); // ciclo finché non si inserisce un sesso corretto
+                else
+                {
+                    sesso = inputSesso[0]; // prendo il primo carattere della stringa inserita 
+                }
+
+            } while (sesso != 'f' && sesso != 'm' && sesso != 'x'); 
 
             // comune di residenza
+
             string comuneResidenza = "";
+
            do
             {
                 Console.WriteLine("Comune di residenza: ");
                 comuneResidenza = Console.ReadLine();
-                if (string.IsNullOrEmpty(comuneResidenza))
+
+                if (string.IsNullOrEmpty(comuneResidenza)) // metodo statico per verificare che il comune di residenza non sia vuoto
                 {
                     Console.WriteLine("Inserire un valore");
                 }
-                } while (string.IsNullOrEmpty(comuneResidenza)); // ciclo finché non si inserisce un comune di residenza corretto
+                } while (string.IsNullOrEmpty(comuneResidenza)); 
             
 
 
@@ -107,6 +126,7 @@ namespace Esame_U1S1
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             decimal redditoAnnuale = 0;
+
             do
             {
                 Console.WriteLine("Reddito annuale: ");
@@ -131,14 +151,14 @@ namespace Esame_U1S1
 
             decimal impostaDaPagare = contribuente.CalcolaImposta();
 
-            // output imposta con menu 
+            // output imposta  
 
             Console.WriteLine($"==================================================");
             Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome}");
             Console.WriteLine($"Nat* il: {contribuente.DataNascita.ToString("dd/MM/yyy")} ({contribuente.Sesso})");
             Console.WriteLine($"Residente in: {contribuente.ComuneResidenza}");
             Console.WriteLine($"Codice fiscale: {contribuente.CodiceFiscale}");
-            Console.OutputEncoding = System.Text.Encoding.UTF8; // per visualizzare il simbolo dell'euro
+            Console.OutputEncoding = System.Text.Encoding.UTF8; 
             Console.WriteLine($"Reddito dichiarato: {contribuente.RedditoAnnuale:C}");
             Console.WriteLine($"IMPOSTA DA VERSARE: {impostaDaPagare:C}");
             Console.WriteLine($"==================================================");
