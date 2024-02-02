af92c6120e0f0c0eb0f47a2bf7426f0f8a7008ab
MelissaCanu <melissamcanu@gmail.com>
2024-02-02 10:41:45 +0100

    input contribuente, new contribuente obj, imposta da pagare w/ metodo calcolaimposta

--- Esame-U1S1/Program.cs
+++ Esame-U1S1/Program.cs
@@ -9,7 +9,41 @@ namespace Esame_U1S1
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // input dati contribuente
+
+            Console.WriteLine("Inserisci i dati del contribuente");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Cognome: ");
+            string cognome = Console.ReadLine();
+
+            Console.WriteLine("Data di nascita: ");
+            DateTime dateTime = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Codice fiscale: ");
+            string codiceFiscale = Console.ReadLine();
+
+            Console.WriteLine("Sesso: ");
+            char sesso = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Comune di residenza: ");
+            string comuneResidenza = Console.ReadLine();
+
+            Console.WriteLine("Reddito annuale: ");
+            decimal redditoAnnuale = decimal.Parse(Console.ReadLine());
+
+            // istanzio oggetto contribuente
+
+            Contribuente contribuente = new Contribuente(nome, cognome, dateTime, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+            // calcolo imposta richiamando il metodo CalcolaImposta della classe Contribuente e stampo il risultato
+
+            decimal impostaDaPagare = contribuente.CalcolaImposta();
+
+
         }
     }
 }
